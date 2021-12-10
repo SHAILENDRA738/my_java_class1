28e7d9c004b7965f6e97d936dfdc8f2c38dd7d0d
SHAILENDRA738 <95921893+SHAILENDRA738@users.noreply.github.com>
2021-12-10 23:42:14 +0530

    hello

--- src/FirstDayClass.java
+++ src/FirstDayClass.java
@@ -2,6 +2,8 @@
 public class FirstDayClass {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("my name is shailendra prasad");
+
 
 
 
