5fc0dd31c84eb39f8c1838805bb6514a8bd556a6
SHAILENDRA738 <95921893+SHAILENDRA738@users.noreply.github.com>
2021-12-13 08:50:05 +0530

    Condition1

--- src/ConditionStatment/ConditionalClass1.java
+++ src/ConditionStatment/ConditionalClass1.java
@@ -1,8 +1,7 @@
 package ConditionStatment;
-
 public class ConditionalClass1 {
     public static void main(String[] args) {
-        int age=18;
+        int age=20;
 
         if (age>18) {
         System.out.println("Eligible For Application");
@@ -11,5 +10,18 @@ public class ConditionalClass1 {
         {
             System.out.println(" Not Eligible For Application");
         }
+
+        String city="BANGALORE";
+        if (city=="BANGALORE") {
+            System.out.println("Person Belong To AGRA");
+        }
+        else
+        {
+            System.out.println("Person Does not Belongs To AGRA");
+
+
+
+                }
+
         }
 }
