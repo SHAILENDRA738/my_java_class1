103d17a44c75bf52181c438ea287bc18ee735c6f
SHAILENDRA738 <95921893+SHAILENDRA738@users.noreply.github.com>
2021-12-13 08:27:53 +0530

    Condition

---
+++ src/ConditionStatment/ConditionalClass1.java
@@ -0,0 +1,15 @@
+package ConditionStatment;
+
+public class ConditionalClass1 {
+    public static void main(String[] args) {
+        int age=18;
+
+        if (age>18) {
+        System.out.println("Eligible For Application");
+    }
+        else
+        {
+            System.out.println(" Not Eligible For Application");
+        }
+        }
+}
