2c297019a692ee2ec525461f1f7d88ea8b6fa86a
SHAILENDRA738 <95921893+SHAILENDRA738@users.noreply.github.com>
2021-12-14 10:46:57 +0530

    Loops2

---
+++ src/LoopsClass/LoopsDemo.java
@@ -0,0 +1,14 @@
+package LoopsClass;
+
+public class LoopsDemo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello SHA" + i);
+        }
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Hello SHA" +i);
+        }
+
+    }
+}
--- src/Loops_Class/LoopsDemo.java
+++
@@ -1,9 +0,0 @@
-package Loops_Class;
-
-public class LoopsDemo {
-    public static void main(String[] args) {
-        for (int i=0; i<=10; i++){
-            System.out.println("Hello SHA"+i);
-        }
-    }
-}
